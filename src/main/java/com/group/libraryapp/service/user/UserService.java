20d99e4ab0c4d93b2b27bb861e7ac932670309fd
billihazero <billihazero@gmail.com>
2024-08-12 17:16:34 +0900

    spring bean 등록

---
+++ src/main/java/com/group/libraryapp/service/user/UserService.java
@@ -0,0 +1,46 @@
+package com.group.libraryapp.service.user;
+
+import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
+import com.group.libraryapp.repository.user.UserRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Service;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    public final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+      this.userRepository = userRepository;
+    }
+
+    public void saveUser(UserCreateRequest request){
+        userRepository.saveUser(request.getName(), request.getAge());
+    }
+
+    public List<UserResponse> getUsers(){
+        return userRepository.getUsers();
+    }
+
+    public void updateUser(UserUpdateRequest request){
+        if(userRepository.isUserNotExist(request.getId())){
+            throw new IllegalArgumentException();
+        }
+        userRepository.updateUserName( request.getId(), request.getName());
+    }
+
+    public void deleteUser(String name){
+        if(userRepository.isUserNotExist(name)){
+            throw new IllegalArgumentException();
+        }
+        userRepository.deleteUser(name);
+    }
+
+}
