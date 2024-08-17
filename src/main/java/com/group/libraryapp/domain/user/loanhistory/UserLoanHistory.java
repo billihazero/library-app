a71bc1f3b4fd6a22ddb0fcbc3645bb47bce98df3
billihazero <billihazero@gmail.com>
2024-08-17 22:04:23 +0900

    반납기능구현

--- src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
+++ src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
@@ -1,5 +1,7 @@
 package com.group.libraryapp.domain.user.loanhistory;
 
+import com.group.libraryapp.domain.user.User;
+
 import javax.persistence.*;
 
 @Entity
@@ -10,17 +12,17 @@ public class UserLoanHistory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id = null;
 
-    @Column
-    private Long userId;
+    @ManyToOne
+    private User user;
 
     private String bookName;
 
     private boolean isReturn;
 
-    public UserLoanHistory(Long userId, String bookName) {
-        this.userId = userId;
+    public UserLoanHistory(User user, String bookName) {
+        this.user = user;
         this.bookName = bookName;
         this.isReturn = false;
     }
